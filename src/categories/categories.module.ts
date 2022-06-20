eda18114090b6135fc5645c464ef2b8101cc231e
Park Min Hyeok <opensource@pmh.codes>
2022-06-20 22:39:52 +0900

    refactor: remove unused components

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { PermissionsModule } from 'src/permissions/permissions.module'
-import { CategoriesController } from './categories.controller'
 import { Categories, Subcategories } from './categories.entities'
 import { CategoriesService } from './categories.service'
 
@@ -10,7 +9,6 @@ import { CategoriesService } from './categories.service'
     TypeOrmModule.forFeature([Categories, Subcategories]),
     PermissionsModule
   ],
-  controllers: [CategoriesController],
   providers: [CategoriesService],
   exports: [CategoriesService]
 })
